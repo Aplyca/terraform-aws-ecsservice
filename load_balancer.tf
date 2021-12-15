c0560c896ad0a8b6deb9038da6e53ddf092c09fc
agomezaplyca <60756439+agomezaplyca@users.noreply.github.com>
2021-12-15 10:13:53 -0500

    Adding alb cognito autentication support (#12)
    * Update variables.tf
    
    * Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -34,9 +34,23 @@ resource "aws_lb_listener_rule" "this" {
   count = length(var.listener_rules)  
   listener_arn =  element(data.aws_alb_listener.this, count.index).arn
 
-  action {
-    type = "forward"
-    target_group_arn = aws_alb_target_group.default.0.arn
+  dynamic "action" {
+    for_each = var.aws_lb_listener_rule_actions
+
+    content {
+      type = action.value.type
+      target_group_arn = lookup(action.value, "type", null) == "forward" ? aws_alb_target_group.default.0.arn : "" 
+
+      dynamic "authenticate_cognito" {
+        for_each = lookup(action.value, "type", null) == "authenticate-cognito" ? list(action.value.type) :[]
+        content {
+          user_pool_arn = action.value.user_pool_arn
+          user_pool_client_id = action.value.user_pool_client_id
+          user_pool_domain    = action.value.user_pool_domain
+        }
+      }
+
+    }
   }
 
   condition {
